3661d55b2bf44da4a0b7edbe75ebafc15b2aaae1
Jeason <2055469819@qq.com>
2021-04-26 08:51:11 +0800

    add detail for data

---
+++ R/data.R
@@ -0,0 +1,27 @@
+#' the annotation file of hug133plus2 array
+#'
+#' A dataset containing the probe id and coresponding gene id of hug133plus2 array
+#'
+#' @format
+#' \describe{
+#'   \item{probe_id}{array probe id}
+#'   \item{symbol}{probe coresponding gene id}
+#' }
+#'
+"anno_hug133plus2"
+
+#' gene length information of human
+#'
+#' The gene length and id information of human acessed from ensembl
+#' @format
+#' @source \url{www.ensembl.org}
+#'
+"human"
+
+#' gene length information of mouse
+#'
+#' The gene length and id information of mouse acessed from ensembl
+#' @format
+#' @source \url{www.ensembl.org}
+#'
+"mouse"
